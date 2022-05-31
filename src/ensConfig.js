183aaea91416ef1a51a0a9fd8839bb94f95f69c9
Yushin Si <siyushin@hotmail.com>
2022-05-31 12:38:46 +0800

    for Polygon Testnet...

--- src/ensConfig.js
+++ src/ensConfig.js
@@ -4,6 +4,11 @@ export const ensConfig = {
 			name: "ESC",
 			ensAddress: "0xb9930eF91BADBC650269c382E3B5877574e6a7fc",
 			tld: "ela"
+		},
+		"80001": {
+			name: "Polygon Testnet",
+			ensAddress: "0x28fE6F94566e39d22C82906b42c17fe9e44362b0",
+			tld: "matic"
 		}
 	}
 };
