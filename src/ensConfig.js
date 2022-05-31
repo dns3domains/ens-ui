86dc3a649bbbaa34d142746ea3db8ebefc587b11
Yushin Si <siyushin@hotmail.com>
2022-05-31 12:52:39 +0800

    for BSC Testnet...

--- src/ensConfig.js
+++ src/ensConfig.js
@@ -9,6 +9,11 @@ export const ensConfig = {
 			name: "Polygon Testnet",
 			ensAddress: "0x28fE6F94566e39d22C82906b42c17fe9e44362b0",
 			tld: "matic"
+		},
+		"97": {
+			name: "BSC Testnet",
+			ensAddress: "0x8AE7762E47eC0bE21c25c5EBB5d6E0B768Fc117d",
+			tld: "bnb"
 		}
 	}
 };
